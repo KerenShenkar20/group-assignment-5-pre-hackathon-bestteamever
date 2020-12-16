feb6f7c4e446238cb8fcf406babe11b32047c3c4
DAVID <tsiba1@walla.co.il>
2020-12-16 23:03:03 +0200

    marge

---
+++ service/model/user.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+// const addressShema = new Schema({
+//     lng: { type: Number },
+//     lat: { type: Number }
+// })
+const UserSchema = new Schema({
+    id: { type: Number },
+    first_name: { type: String },           ///,required: true
+    last_name:{ type:String},
+    email:{type:String},
+    gender:{type:String},
+    avatar:{type:String},
+    color:{type:String},
+    job:{type:String}
+}, { conection: "Users" });
+const User = model("User", UserSchema);
+
+module.exports = User;
