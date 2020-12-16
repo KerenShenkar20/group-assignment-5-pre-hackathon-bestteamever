16e52e7a11a15138376c5f7bab4fab6a00ae4bf8
Moriel Turjeman <moriel99@pop-os.localdomain>
2020-12-17 00:18:09 +0200

    connected user router

--- service/routers/userRouter.js
+++ service/routers/userRouter.js
@@ -8,3 +8,5 @@ userRouter.get('/:id', userDbcontroller.getUser);
 userRouter.post('/', userDbcontroller.addUser);
 userRouter.put('/:id', userDbcontroller.updateUser);
 userRouter.delete('/:id', userDbcontroller.deleteUser);
+
+exports.UserRouter = userRouter;
