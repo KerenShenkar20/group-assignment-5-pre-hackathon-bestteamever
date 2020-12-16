75c704c66280f538b66405b76f3c81518e9bcd4f
dekelme <61948586+dekelme@users.noreply.github.com>
2020-12-16 23:13:33 +0200

    Update user.ctrl.js

--- service/conrollers/user.ctrl.js
+++ service/conrollers/user.ctrl.js
@@ -5,7 +5,7 @@ exports.userDbcontroller = {
         const id = req.params.id
         User.find({ id: id }).
             then(docs => { res.json(docs) })
-            .catch(err => console.log('Eroor getting the data from db: ${err}'));
+            .catch(err => console.log('Erorr getting the data from db: ${err}'));
     },
     getUsers(req, res) {
 
@@ -15,9 +15,28 @@ exports.userDbcontroller = {
 
     },
     updateUser(req, res) {
-
+        var key = req.body.id
+        for(i in usersjson)
+        {
+           if (userDbcontroller[i]["id"] == key){
+            userDbcontroller[i]["first_name"] = req.body.first_name
+            userDbcontroller[i]["last_name"] = req.body.last_name 
+            userDbcontroller[i]["email"] = req.body.email 
+            userDbcontroller[i]["gender"] = req.body.gender 
+            userDbcontroller[i]["country"] = req.body.country 
+            userDbcontroller[i]["color"] = req.body.color
+               res.send(`User ${key} has been updated`).on('finish');
+            }
+        }
+        res.send("user dose not exiest").on('finish');
     },
     deleteUser(req, res) {
-
+        var key = req.params.id    
+        for(i in userDbcontroller){
+           if(key == userDbcontroller[i]["id"]){
+               delete userDbcontroller[i]
+               res.send(`User ${key} has been deleted`).on('finish');
+           }
+        }
     }
 }
