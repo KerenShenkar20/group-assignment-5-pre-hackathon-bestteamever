4ddc331dbdcd3951ff7723c90b06ee600778c1df
Moriel Turjeman <moriel99@pop-os.localdomain>
2020-12-17 17:02:53 +0200

    added put to cors

--- service/server.js
+++ service/server.js
@@ -8,6 +8,7 @@ const {UserRouter} = require("./routers/userRouter")
 app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods','POST, PUT, GET, DELETE, OPTIONS')
     res.set('Content-Type', 'application/json');
     next();
 });
