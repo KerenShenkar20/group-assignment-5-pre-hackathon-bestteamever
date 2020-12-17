f25f38a227291812b001523b0bbfc0db68bc7116
LinoyChubok <linoychubok@gmail.com>
2020-12-17 13:44:08 +0200

    recreateUsersTable

--- client/js/logics.js
+++ client/js/logics.js
@@ -1,6 +1,6 @@
-$(function() {
+$(document).ready(function () {
     getAllUsers();
-    userOperationsListeners();
+    // operationsListeners();
 });
 
 function getAllUsers() {
@@ -9,11 +9,18 @@ function getAllUsers() {
         type: 'GET',
         success: function(users) {
             recreateUsersTable(users);
-            console.log()
         }
     });
 }
 
+function getUserById(userId) {
+    $.ajax({
+        url: `http://localhost:3000/api/users/${userId}`,
+        type: 'GET',
+        success: function (user) {
+        }
+    });
+}
 
 function updateUsersById(userId) {
     $.ajax({
@@ -25,14 +32,28 @@ function updateUsersById(userId) {
     });
 }
 
+function appendTableRow(id, first_name, last_name, email, gender, avatar, job)
+{
+    let tableRow = "<tr><td>$userId</td><td>$first_name</td><td>$last_name</td><td>$email</td><td>$gender</td><td><img src='$avatar' alt='Avatar' class='avatar'></td><td>$job</td></tr>";
+    tableRow = tableRow.replace("$userId",id);
+    tableRow = tableRow.replace("$first_name",first_name);
+    tableRow = tableRow.replace("$last_name",last_name);        
+    tableRow = tableRow.replace("$email",email);        
+    tableRow = tableRow.replace("$gender",gender);        
+    tableRow = tableRow.replace("$avatar",avatar);        
+    tableRow = tableRow.replace("$job",job);        
+    $("#usersTable tbody").append(tableRow);
+}
 
-
-function recreateRestaurantsTable(rests) {
-    $("table").empty().remove();
-
-    // Continue
-
-    $('#users-list').append("");
+function recreateUsersTable(users) {
+    $("#usersTable tbody").empty().remove();
+    const usersLen = users.length;
+    if (usersLen) {
+        $('table').append('<tbody></tbody>');
+        for (let i = 0; i < usersLen; i++) {
+            appendTableRow(users[i].id, users[i].first_name, users[i].last_name, users[i].email, users[i].gender, users[i].avatar, users[i].job)
+        }
+    } 
 }
 
 function userOperationsListeners() {
