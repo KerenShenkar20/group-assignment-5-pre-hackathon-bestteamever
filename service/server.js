7b82fa4cf0cc5032c28db57adce6f0247f4497b1
Moriel Turjeman <moriel99@pop-os.localdomain>
2020-12-17 13:04:46 +0200

    cors fix

--- service/server.js
+++ service/server.js
@@ -7,7 +7,7 @@ const {UserRouter} = require("./routers/userRouter")
 
 app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Origin', 'Origin, X-requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-requested-With, Content-Type, Accept');
     res.set('Content-Type', 'application/json');
     next();
 });
